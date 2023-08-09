54af5a97708cec725ff303f36e36d4c4ad37d812
Avery Miller <averymiller2060@gmail.com>
2023-08-09 18:55:34 -0400

    implemented POST route for userRoutes to create a new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,34 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+
+});
+
 module.exports = router;
