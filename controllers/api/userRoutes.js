6aa723ad76fd087dc5088f5957269e90375d84bf
Avery Miller <averymiller2060@gmail.com>
2023-08-09 19:00:21 -0400

    implemented POST route for userRoutes to logout a user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -55,7 +55,13 @@ router.post('/login', async (req, res) => {
 
 // Logout
 router.post('/logout', (req, res) => {
-
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
