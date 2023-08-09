d30b9bba72050957364db81cd7c6cf81f7f92f37
Avery Miller <averymiller2060@gmail.com>
2023-08-09 18:59:45 -0400

    implemented POST route for userRoutes to login a user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -23,7 +23,34 @@ router.post('/', async (req, res) => {
 
 // Login
 router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
 
+    if(!userData) {
+      res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if(!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      console.log('File: user-routes.js ~ line 57 ~ req.session.save ~ req.session.cookie', req.session.cookie);
+      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // Logout
