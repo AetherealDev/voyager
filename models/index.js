69d1f03b92a1527655f90c72552817cb62199caf
millerdotkyle <kylenotfound@gmail.com>
2023-08-14 19:47:16 -0400

    GET following almost working
    page now is rendered but missing content

--- models/index.js
+++ models/index.js
@@ -3,4 +3,12 @@ const Location = require('./Location');
 const Post = require('./Post');
 const Review = require('./Review');
 
+User.hasMany(Post, {
+    foreignKey: 'userId'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'userId'
+});
+
 module.exports = { User, Location, Review, Post };
