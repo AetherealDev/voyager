8f9f1a768df497634eba12cd531a4ea557567b7f
millerdotkyle <kylenotfound@gmail.com>
2023-08-15 15:54:21 -0400

    added id

--- controllers/followingRoutes.js
+++ controllers/followingRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
                 include: [
                     {
                         model: User,
-                        attributes: ['username'],
+                        attributes: ['id', 'username'],
                     },
                 ] ,
             });
