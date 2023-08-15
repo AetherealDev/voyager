acecff55c027aa6f07859b37d952e50b78557178
millerdotkyle <kylenotfound@gmail.com>
2023-08-14 20:28:52 -0400

    only pass user.username, debugging
    posts not passing to be rendered

--- controllers/followingRoutes.js
+++ controllers/followingRoutes.js
@@ -1,17 +1,20 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
-// const Post = require('../models/Post');
 
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll(
             { 
                 include: [
-                    {model: User}
-                ] 
+                    {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                ] ,
             });
         
         const posts = postData.map((post) => post.get( {plain: true})); 
+        console.log(posts, 'HELLLO');
         res.render('following', {posts});
     } catch (err) {
         console.error();
