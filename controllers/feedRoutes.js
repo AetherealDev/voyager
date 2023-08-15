f02c0c2d7f8df8d02b1bffbbeb34ed0866871ef7
Avery Miller <averymiller2060@gmail.com>
2023-08-15 11:27:04 -0400

    added GET routes for feed view

--- controllers/feedRoutes.js
+++ controllers/feedRoutes.js
@@ -1,4 +1,54 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+  res.redirect('/recent');
+});
+
+router.get('/recent', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const posts = postData.map((post) =>
+      post.get({ plain: true })
+    );
+
+    res.render('feed', { posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/popular', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+      order: [
+        ['upvote', 'DESC'],
+      ],
+    });
+
+    const posts = postData.map((post) =>
+      post.get({ plain: true })
+    );
+
+    res.render('feed', { posts });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
