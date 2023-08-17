df306217b60f18652ed870ff35ec585db0bba56c
millerdotkyle <kylenotfound@gmail.com>
2023-08-17 16:18:55 -0400

    removed unused nav code

--- public/js/navmenu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navlinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  } 
